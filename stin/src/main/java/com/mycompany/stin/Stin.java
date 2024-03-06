44a794e36110a7a73dc62d1ca59e6670e1d190ae
Michal_Pokorny <michal.pokorny@tul.cz>
2024-03-06 13:10:05 +0100

    initial commit

---
+++ stin/src/main/java/com/mycompany/stin/Stin.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.stin;
+
+/**
+ *
+ * @author michal.pokorny
+ */
+public class Stin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
